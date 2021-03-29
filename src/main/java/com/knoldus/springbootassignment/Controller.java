095c707f182522699209aa6f8c22af747ba01013
shashank-knoldus <shashank.goyal@knoldus.com>
2021-03-29 23:29:52 +0530

    springbootassignment

---
+++ src/main/java/com/knoldus/springbootassignment/Controller.java
@@ -0,0 +1,28 @@
+package com.knoldus.springbootassignment;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Controller {
+  /**
+   * This method will give the current time on calling /status API>
+   * @return Current Time
+   */
+
+  @GetMapping("/status")
+  public String getCurrentTime() {
+	Calendar cal = Calendar.getInstance();
+	Date date = cal.getTime();
+	DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	String formattedDate = dateFormat.format(date);
+	return "Running,{" + formattedDate + "}";
+
+  }
+
+}
---
+++ src/test/java/com/knoldus/springbootassignment/ControllerTest.java
@@ -0,0 +1,25 @@
+package com.knoldus.springbootassignment;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ControllerTest {
+
+  @Test
+  public void getCurrentTime() {
+
+	Controller c = new Controller();
+	Calendar cal = Calendar.getInstance();
+	Date date = cal.getTime();
+	DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	String result = "Running,{" + dateFormat.format(date) + '}';
+
+	Assert.assertEquals(result, c.getCurrentTime());
+
+  }
+}
